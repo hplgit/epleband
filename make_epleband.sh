98f0229947a67a77d842f02169f19f480b853098
Hans Petter Langtangen <hpl@simula.no>
2013-12-30 11:03:37 +0100

    Updates for 2014 party.

--- make_epleband.sh
+++ make_epleband.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
-year=13
+if [ $# -gt 0 ]; then
+  year=$1
+else
+  year=14
+fi
 cd 20$year
 
-doconce format html epleband${year} -DLYRICS
+doconce format html epleband${year} -DLYRICS -DTAB
 mv epleband${year}.html epleband${year}_lyrics.html
 doconce format html epleband${year}
+exit
 
-doconce format pdflatex epleband${year} -DLYRICS
+doconce format pdflatex epleband${year} -DLYRICS -DTAB
 doconce ptex2tex epleband${year}  -DA4PAPER
 pdflatex epleband${year}
 mv epleband${year}.pdf epleband${year}_lyrics.pdf
