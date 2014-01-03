73454faaa8f4de11c025d3353c33dbb75f2b2aa3
Hans Petter Langtangen <hpl@simula.no>
2014-01-03 12:59:19 +0100

    updates

--- make_epleband.sh
+++ make_epleband.sh
@@ -6,21 +6,37 @@ else
 fi
 cd 20$year
 
-doconce format html epleband${year} -DLYRICS -DTAB
-mv epleband${year}.html epleband${year}_lyrics.html
-doconce format html epleband${year}
-exit
+# Extended info HTML
+doconce format html epleband${year} -DLYRICS -DTAB -DMOVIES --html_style=bloodish --encoding=utf-8
+mv epleband${year}.html epleband${year}_lyrics_tab_youtube.html
+doconce split_html epleband${year}_lyrics_tab_youtube.html
 
-doconce format pdflatex epleband${year} -DLYRICS -DTAB
+# Extended info HTML, one big file, tab, but no movies
+doconce format html epleband${year} -DLYRICS -DTAB --html_style=bloodish --encoding=utf-8
+mv epleband${year}.html epleband${year}_lyrics_tab.html
+
+# Extended info HTML, one big file, no movies
+doconce format html epleband${year} -DLYRICS --html_style=bloodish --encoding=utf-8
+mv epleband${year}.html epleband${year}_lyrics.html
+
+# Compact info HTML
+doconce format html epleband${year} --html_style=bloodish --encoding=utf-8
+
+# Extended info PDF
+doconce format pdflatex epleband${year} -DLYRICS -DTAB  # drop movies in pdf
 doconce ptex2tex epleband${year}  -DA4PAPER
 pdflatex epleband${year}
-mv epleband${year}.pdf epleband${year}_lyrics.pdf
+mv epleband${year}.pdf epleband${year}_lyrics_tab.pdf
 
+# Compact info PDF
 doconce format pdflatex epleband${year}
 doconce ptex2tex epleband${year}  -DA6PAPER
 pdflatex epleband${year}
 mv epleband${year}.pdf epleband${year}_mobil.pdf
 
+# Compact info A6 PDF for mobile phone
 doconce format pdflatex epleband${year}
 doconce ptex2tex epleband${year}  -DA4PAPER
 pdflatex epleband${year}
+
+doconce format html index --html_style=bloodish
