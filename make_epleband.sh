90203d0cbdb2c5ebc3dcc48955639d900ee31eb9
Hans Petter Langtangen <hpl@simula.no>
2014-01-15 12:30:15 +0100

    updates

--- make_epleband.sh
+++ make_epleband.sh
@@ -6,46 +6,61 @@ else
 fi
 cd 20$year
 
-# Extended info HTML
-doconce format html epleband${year} -DLYRICS -DTAB -DMOVIES --html_style=bloodish --encoding=utf-8
-mv epleband${year}.html epleband${year}_lyrics_tab_youtube.html
-doconce split_html epleband${year}_lyrics_tab_youtube.html
+function system {
+  "$@"
+  if [ $? -ne 0 ]; then
+    echo "make.sh: unsuccessful command $@"
+    echo "abort!"
+    exit 1
+  fi
+}
 
-doconce format html epleband${year} -DLYRICS -DTAB -DTAB_SOLO -DMOVIES --html_style=bloodish --encoding=utf-8
+# Extended info HTML
+system doconce format html epleband${year} -DLYRICS -DTAB -DMOVIES --html_style=bloodish --encoding=utf-8
+mv epleband${year}.html epleband${year}_lyrics_tab_youtube.html
+system doconce split_html epleband${year}_lyrics_tab_youtube.html
+
+system doconce format html epleband${year} -DLYRICS -DTAB -DTAB_SOLO -DMOVIES --html_style=bloodish --encoding=utf-8
 if [ $? -ne 0 ]; then exit 1; fi
 mv epleband${year}.html epleband${year}_lyrics_tab_youtube_solo.html
-doconce split_html epleband${year}_lyrics_tab_youtube_solo.html
-doconce format html epleband${year} -DLYRICS -DTAB -DTAB_SOLO --html_style=bloodish --encoding=utf-8
+system doconce split_html epleband${year}_lyrics_tab_youtube_solo.html
+system doconce format html epleband${year} -DLYRICS -DTAB -DTAB_SOLO --html_style=bloodish --encoding=utf-8
 mv epleband${year}.html epleband${year}_lyrics_tab_solo.html
 
 
 # Extended info HTML, one big file, tab, but no movies
-doconce format html epleband${year} -DLYRICS -DTAB --html_style=bloodish --encoding=utf-8
+system doconce format html epleband${year} -DLYRICS -DTAB --html_style=bloodish --encoding=utf-8
 mv epleband${year}.html epleband${year}_lyrics_tab.html
 
 # Extended info HTML, one big file, no movies
-doconce format html epleband${year} -DLYRICS --html_style=bloodish --encoding=utf-8
+system doconce format html epleband${year} -DLYRICS --html_style=bloodish --encoding=utf-8
 mv epleband${year}.html epleband${year}_lyrics.html
 
 # Compact info HTML
-doconce format html epleband${year} --html_style=bloodish --encoding=utf-8
+system doconce format html epleband${year} --html_style=bloodish --encoding=utf-8
 
 # Extended info PDF
-doconce format pdflatex epleband${year} -DLYRICS -DTAB  # drop movies in pdf
-doconce ptex2tex epleband${year}  -DA4PAPER
+system doconce format pdflatex epleband${year} -DLYRICS
+doconce replace '\textbf{Chords' '\vspace{0.5cm}\par\noindent\textbf{Chords' epleband${year}.p.tex
+doconce replace '\section{' '\clearpage\section{' epleband${year}.p.tex
+system doconce ptex2tex epleband${year}  -DA4PAPER -DHELVETICA
 pdflatex epleband${year}
-mv epleband${year}.pdf epleband${year}_lyrics_tab.pdf
+pdflatex epleband${year}
+pdflatex epleband${year}
+mv epleband${year}.pdf epleband${year}_lyrics.pdf
 
 # Compact info PDF
-doconce format pdflatex epleband${year}
-doconce ptex2tex epleband${year}  -DA6PAPER
+system doconce format pdflatex epleband${year}
+doconce replace '\textbf{Chords' '\vspace{0.5cm}\par\noindent\textbf{Chords' epleband${year}.p.tex
+system doconce ptex2tex epleband${year}  -DA6PAPER -DHELVETICA
 pdflatex epleband${year}
 mv epleband${year}.pdf epleband${year}_mobil.pdf
 
 # Compact info A6 PDF for mobile phone
-doconce format pdflatex epleband${year}
-doconce ptex2tex epleband${year}  -DA4PAPER
+system doconce format pdflatex epleband${year}
+doconce replace '\tableofcontents' '' epleband${year}.p.tex
+system doconce ptex2tex epleband${year}  -DA4PAPER -DHELVETICA
 pdflatex epleband${year}
 
-
-doconce format html index --html_style=bloodish
+# Compact HTML
+system doconce format html index --html_style=bloodish
